61f0c4f09613bf1842164eb4873d46d2f2a9467f
Puiu Ioan Alexandru <ionutpuiu010405@gmail.com>
2025-05-15 15:30:37 +0300

    Customer and Employee UI implemented

---
+++ Customer/UI/Customer_UI.h
@@ -0,0 +1,18 @@
+//
+// Created by ionut on 5/15/2025.
+//
+
+#ifndef CUSTOMER_UI_H
+#define CUSTOMER_UI_H
+#include <string>
+
+
+class Customer_UI {
+public:
+    std::string main_menu();
+
+};
+
+
+
+#endif //CUSTOMER_UI_H
---
+++ Employee/UI/EmployeeUI.h
@@ -0,0 +1,23 @@
+//
+// Created by ionut on 5/15/2025.
+//
+
+#ifndef EMPLOYEEUI_H
+#define EMPLOYEEUI_H
+#include <string>
+
+
+class EmployeeUI {
+public:
+  static std::string main_menu();
+
+  static std::string order_menu();
+
+  static std::string account_menu();
+
+  static std::string user_menu();
+};
+
+
+
+#endif //EMPLOYEEUI_H
